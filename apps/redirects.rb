f725aa2e0ec1ccbc33b72bba93253e655affa5c4
Trevor Turk <trevorturk@gmail.com>
2010-05-08 11:15:14 -0500

    trailing slash optional for redirects

--- apps/redirects.rb
+++ apps/redirects.rb
@@ -1,37 +1,37 @@
 class Redirects < Sinatra::Base
-  get '/click/' do
+  get '/click/?' do
     redirect 'http://clickthatbutton.com/', 301
   end
 
-  get '/eldorado/' do
+  get '/eldorado/?' do
     redirect 'http://github.com/trevorturk/eldorado/', 301
   end
 
-  get '/flowcoder/' do
+  get '/flowcoder/?' do
     redirect 'http://flowcoder.com/', 301
   end
 
-  get '/h8ter/' do
+  get '/h8ter/?' do
     redirect 'http://h8ter.org/', 301
   end
 
-  get '/kzak/' do
+  get '/kzak/?' do
     redirect 'http://github.com/trevorturk/kzak/', 301
   end
 
-  get '/pygments/' do
+  get '/pygments/?' do
     redirect 'http://pygments.appspot.com/', 301
   end
 
-  get '/reelroulette/' do
+  get '/reelroulette/?' do
     redirect 'http://reelroulette.net/', 301
   end
 
-  get '/static/' do
+  get '/static/?' do
     redirect 'http://github.com/trevorturk/static/', 301
   end
 
-  get '/wordpress/' do
+  get '/wordpress/?' do
     redirect 'http://wordpress.org/extend/plugins/profile/trevorturk/', 301
   end
 end
