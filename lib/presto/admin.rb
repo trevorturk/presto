99c1d08e981ae0f546f57e608a43a78408b6ab8a
Trevor Turk <trevorturk@gmail.com>
2010-05-15 13:39:24 -0500

    http basic for admin

--- lib/presto/admin.rb
+++ lib/presto/admin.rb
@@ -7,6 +7,13 @@ class Presto::Admin < Sinatra::Base
   set :public, './public/admin'
   set :views, './public/admin'
 
+  if ENV['RACK_ENV'] == 'production'
+    raise 'no user/pass' unless ENV['presto_user'] && ENV['presto_pass']
+    use Rack::Auth::Basic do |user, pass|
+      [user, pass] == [ENV['presto_user'], ENV['presto_pass']]
+    end
+  end
+
   get '/' do
     erb :index
   end
