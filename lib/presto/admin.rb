084ea5ef72cb60bef2c010f0a55bf4c3f4d918a3
Trevor Turk <trevorturk@gmail.com>
2010-05-15 13:09:33 -0500

    admin base

--- lib/presto/admin.rb
+++ lib/presto/admin.rb
@@ -1,19 +1,13 @@
 require 'sinatra/base'
 require 'lib/presto/models'
-require 'lib/presto/helpers'
 
 class Presto::Admin < Sinatra::Base
   set :raise_errors, true
+  set :public, './public/admin'
   set :views, './public/admin'
   set :database, ENV['DATABASE_URL']
 
-  helpers Presto::Helpers
-
   get '/' do
-    'hello'
-  end
-
-  not_found do
-    redirect '/presto/?not_found=1', 302
+    erb :index
   end
 end
